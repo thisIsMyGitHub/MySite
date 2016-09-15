baf9feebd46579d8b52b370140e828b025ae3ef6
thisIsMyGitHub <omegasayan@outlook.com>
2016-09-15 14:52:16 +1200

    update3

--- js/basic.ts
+++ js/basic.ts
@@ -1,25 +1,25 @@
-var pageheader = $("#page-header")[0]; 
+var pageheader = $("#page-header")[0]; //note the [0], jQuery returns an object, so to get the html DOM object we need the first item in the object
 var pagecontainer = $("#page-container")[0]; 
-var imgSelector = $("#my-file-selector"); 
+var imgSelector = $("#my-file-selector"); //You dont have to use [0], however this just means whenever you use the object you need to refer to it with [0].
 //var refreshbtn = $("#refreshbtn"); 
 
 
-function processImage(callback) {
-    var file = imgSelector.get(0).files[0]; 
+function processImage(callback) : void{
+    var file = imgSelector.get(0).files[0]; //get(0) is required as imgSelector is a jQuery object so to get the DOM object, its the first item in the object. files[0] refers to the location of the photo we just chose.
      var reader = new FileReader();
     if (file) {
-        reader.readAsDataURL(file); 
+        reader.readAsDataURL(file); //used to read the contents of the file
     }
     else {
         console.log("Invalid file");
     }
     reader.onloadend = function () {
-    
+        //After loading the file it checks if extension is jpg or png and if it isnt it lets the user know.
         if (!file.name.match(/\.(jpg|jpeg|png)$/)) {
             pageheader.innerHTML = "Please upload an image file (jpg or png).";
         }
         else {
-        
+            //if file is photo it sends the file reference back up
             callback(file);
         }
     };
@@ -35,7 +35,7 @@ function changeUI(cap) {
 
 
 
-    function sendRequest(file, callback) {
+    function sendEmotionRequest(file, callback): void {
     $.ajax({
         url: "https://api.projectoxford.ai/vision/v1.0/describe?maxCandidates=1",
         beforeSend: function (xhrObj) {
@@ -48,7 +48,7 @@ function changeUI(cap) {
         processData: false
     })
         .done(function (data) {
-            if (data.description.length != 0) { 
+            if (data.description.length != 0) { // if a face is detected
                 
                 var cap = data.description.captions[0].text;
                 callback(cap);
@@ -62,12 +62,12 @@ function changeUI(cap) {
         });
 }
 imgSelector.on("change", function () {
-    pageheader.innerHTML = "Analysing the Image..."; 
-    processImage(function (file) { 
+    pageheader.innerHTML = "Analysing the Image"; //good to let your user know something is happening!
+    processImage(function (file) { //this checks the extension and file
 	
-        sendRequest(file, function (capp) { 
+        sendEmotionRequest(file, function (capp) { //here we send the API request and get the response
 
-            changeUI(capp); 
+            changeUI(capp); //time to update the web app, with their emotion!
 
             //Done!!
         });
